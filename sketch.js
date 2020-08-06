b9c34134e60078f5e60efce9c30be296fc5a95e5
AnshulKumarasamy <68804325+AnshulKumarasamy@users.noreply.github.com>
2020-08-06 23:12:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,43 +1,51 @@
-//Global Variables
 var PLAY = 1;
 var END = 0;
 var gameState = PLAY; 
 
 var canvas;
-var monkey;
-var obstacles;
-var banana;
-var ground;
-var scene;
-var gameOver;
-var resart;
-var SurvivalTime = 0;
+var monkey, monkeyIMG, monkeyStop;
+var obstacles, obstacleIMG;
+var banana, bananaIMG;
+var ground, groundIMG;
+var scene, sceneIMG;
+var gameOver, gameOverIMG;
+var restart, restartIMG;
+var score = 0;
 
-// function preload(){
-//   monkey = loadAnimation ("Monkey_01.png", "Monkey_02.png", "Monkey_03.png","Monkey_04.png", "Monkey_05.png", 
-//   "Monkey_06.png", "Monkey_07.png", "Monkey_08.png", "Monkey_09.png", "Monkey_10.png");
+ function preload(){
+  monkeyIMG=loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png", "Monkey_05.png","Monkey_06.png", "Monkey_07.png", "Monkey_08.png", "Monkey_09.png", "Monkey_10.png");
+   monkeyStop= loadAnimation("Monkey_05.png");
+   obstaclesIMG = loadImage ("stone.png"); 
+   bananaIMG = loadImage ("banana.png");
+   sceneIMG = loadImage ("jungle.jpg");
+   gameOverIMG= loadImage ("gameOver.png");
+   restartIMG = loadImage ("restart.png");
   
-//   obstacles = loadImage ("stone.png"); 
-//   banana = loadImage ("banana.png");
-//   scene = loadImage ("jungle.jpg");
-//   gameOver = loadImage ("gameOver.png");
-//   restart = loadImage ("restart.png");
-  
-// }
+ }
 
 
 function setup() {
   canvas = createCanvas(1200,800);
   
-   monkey = createSprite(200,600,100,75);
-  // monkey.addAnimation(monkey);
-    
    ground= createSprite(600,400,1200,10);
+   ground.addImage(groundIMG);
    ground.velocityX=-6; 
    ground.x=ground.width/2;
-
+   ground.visible=false;
+  
+   scene= createSprite(600,400,1200,800);
+   scene.addImage(sceneIMG);
+   scene.velocityX=-6; 
+   scene.x=scene.width/2;
+  
+   monkey = createSprite(200,600,100,75);
+   monkey.addAnimation(monkeyIMG);
+    
    gameOver= createSprite(200,200,10,10);
+   gameOver.addImage(gameOverIMG);
+  
    restart= createSprite(200,340,10,10);
+   restart.addImage(restartIMG);
   
   BananaGroup = new Group ();
   ObstacleGroup = new Group();
@@ -46,7 +54,9 @@ function setup() {
 
 function draw(){
  background(255);
-  
+ scene.velocityX=-6; 
+ scene.x=scene.width/2;
+
    if ( gameState === PLAY){
     
     if (keyDown("space")){
@@ -62,16 +72,14 @@ function draw(){
     if (BananaGroup.isTouching(monkey)) {
       BananaGroup.setLifetimeEach(0); 
     }
-    
-    stroke("black");
+     
+    stroke("white");
     textSize(20);
-    fill("black");
-    SurvivalTime = round (frameCount/ frameRate());
-    text("Survival Time: "+ SurvivalTime,100,50);
+    fill("white");
+    text("score: "+ score,500,50); 
     
     if (ObstacleGroup.isTouching(monkey)) {
       gameState = END;
-      
     }
     
     Banana();
@@ -89,22 +97,20 @@ function draw(){
     monkey.velocityX(0);
     monkey.velocityY(0);   
     
-    //monkey.changeAnimation("Monkey_05.png");
+    monkey.changeAnimation(monkeyStop);
     
     stroke("black");
     textSize(20); 
     fill("black");
-    text("Survival Time: "+0,100,50);
-    
-    drawSprites();
-    
+    text("score: "+0,100,50); 
   }  
+  drawSprites();
 }
 
 function Banana(){
   if(frameCount%80 === 0){
     var banana = createSprite (600, random (200,250),100,75);
-    //banana.addAnimation(banana);
+    banana.addImage(bananaIMG);
     banana.scale=0.05;
     banana.velocityX=-4;
     banana.lifetime=110;
@@ -115,7 +121,7 @@ function Banana(){
 function Obstacles (){
   if (frameCount%200 === 0) {
     var obstacles = createSprite (600,250,100,75);
-   // obstacles.addAnimation(obstacles);
+    obstacles.addImage(obstaclesIMG);
     obstacles.scale=0.15;
     obstacles.velocityX=-4;
     obstacles.lifetime=110;
